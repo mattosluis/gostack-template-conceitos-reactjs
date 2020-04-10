2cb2fb094d4e556fa12e421992d236a0c0b98fcb
Luis Felipe Mattos <luis.brito@sc.senai.br>
2020-04-10 19:46:34 -0300

    feat - features requested by the challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api"
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => setRepositories(response.data));
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('/repositories', {
+      title: "Bootcamp desafio React",
+      owner: "Akinn Monstro",
+      url: "http://composer.org",
+    })
+
+    setRepositories([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`/repositories/${id}`);
+
+    const newRepositories = repositories.filter(repository => repository.id !== id)
+
+    setRepositories(newRepositories)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { repositories.map(repository => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+    
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
